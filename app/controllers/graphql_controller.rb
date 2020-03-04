847bdab2f2f9876a3ba7f5a43cac2331f801dc33
nzeroberto <nzeroberto@gmail.com>
2020-03-04 03:56:19 -0500

    solveerror

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,12 +1,12 @@
 class GraphqlController < ApplicationController
-  def execute
+ def execute
     variables = ensure_hash(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
     context = {
       # Query context goes here, for example:
       # current_user: current_user,
-      blog: Blog.first
+      blog: Blog.last
     }
     result = NginxTestAppSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
